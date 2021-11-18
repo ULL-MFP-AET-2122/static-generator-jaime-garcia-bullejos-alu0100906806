fe6180df33a45560ffa72d332765646eac3c975e
Jaime García Bullejos <alu0100906806@ull.edu.es>
2021-11-18 19:00:35 +0000

    new-deploy

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Yo",
+        "excerpt":"Jaime García Bullejos   Imagen   As someone said:     You can’t wait until life isn’t hard anymore before you decide to be happy    Formación     Master profesorado   Grado náutica   Linkedin: Linkedin   Lineas de código   &lt;addr&gt;   function fancyAlert(arg) {   if(arg) {     $.facebox({div:'#foo'})   } }   El número de alumnos es 20  ","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/yo",
+        "teaser": null
+      },{
+        "title": "Aprendiendo a usar un generador estático",
+        "excerpt":"Por fin es jueves vamos a ver si esto funciona Y hoy por lo menos el cañon a funcionado … y el titulo es Aprendiendo a usar un generador estático Y ya casi acabamos estamos a martes Me gusta mucho comer naranjas Me gusta mucho comer peras Me gusta mucho...","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/clase",
+        "teaser": null
+      },{
+        "title": "clase",
+        "excerpt":"hola que tal  ","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/clase2",
+        "teaser": null
+      },{
+        "title": "Latex",
+        "excerpt":"Hemos puesto un codigo en layouts / default y ahora podemos usar formulas en toda la web. \\[f(x) = \\int_{-\\infty}^\\infty \\hat f(\\xi)\\,e^{2 \\pi i \\xi x} \\,d\\xi\\] Datos del archivo csv {“First name”=&gt;”John”, “Last name”=&gt;”Doe”, “Age”=&gt;”35”, “Location”=&gt;”United States”}{“First name”=&gt;”Jane”, “Last name”=&gt;”Doe”, “Age”=&gt;”29”, “Location”=&gt;”France”}{“First name”=&gt;”Jack”, “Last name”=&gt;”Hill”, “Age”=&gt;”25”, “Location”=&gt;”Australia”} Tabla csv...","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/Latex",
+        "teaser": null
+      }]

c1681f51026dfa832d8d1fa3514f1e00cd96ffd4
Jaime García Bullejos <alu0100906806@ull.edu.es>
2021-11-20 11:31:28 +0000

    cambios automaticos que aparecieron al abrir gitpod

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Yo",
-        "excerpt":"Jaime García Bullejos   Imagen   As someone said:     You can’t wait until life isn’t hard anymore before you decide to be happy    Formación     Master profesorado   Grado náutica   Linkedin: Linkedin   Lineas de código   &lt;addr&gt;   function fancyAlert(arg) {   if(arg) {     $.facebox({div:'#foo'})   } }   El número de alumnos es 20  ","categories": ["blog"],
+        "excerpt":"Jaime García Bullejos   Imagen   As someone said:     You can’t wait until life isn’t hard anymore before you decide to be happy    Formación     Master profesorado   Grado náutica   Linkedin: Linkedin  ","categories": ["blog"],
         "tags": [],
         "url": "/blog/yo",
         "teaser": null
@@ -11,15 +11,39 @@ var store = [{
         "url": "/docencia/clase",
         "teaser": null
       },{
-        "title": "clase",
-        "excerpt":"hola que tal  ","categories": ["docencia"],
-        "tags": [],
-        "url": "/docencia/clase2",
-        "teaser": null
-      },{
         "title": "Latex",
         "excerpt":"Hemos puesto un codigo en layouts / default y ahora podemos usar formulas en toda la web. \\[f(x) = \\int_{-\\infty}^\\infty \\hat f(\\xi)\\,e^{2 \\pi i \\xi x} \\,d\\xi\\] Datos del archivo csv {“First name”=&gt;”John”, “Last name”=&gt;”Doe”, “Age”=&gt;”35”, “Location”=&gt;”United States”}{“First name”=&gt;”Jane”, “Last name”=&gt;”Doe”, “Age”=&gt;”29”, “Location”=&gt;”France”}{“First name”=&gt;”Jack”, “Last name”=&gt;”Hill”, “Age”=&gt;”25”, “Location”=&gt;”Australia”} Tabla csv...","categories": ["docencia"],
         "tags": [],
         "url": "/docencia/Latex",
+        "teaser": null
+      },{
+        "title": "¿Qué es la educación?",
+        "excerpt":"La educación como producto sería el resultado final, como por ejemplo: un título académico. La educación como proceso serían todas las clases que uno va dando durante una formación concreta. La educación como medio sería la forma de aprender algo para un fin que no es el de aprender simplemente,...","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/relaciones_tarea1",
+        "teaser": null
+      },{
+        "title": "¿Se puede considerar, según Esteve 2010, que el siguiente docmuental es educactivo?",
+        "excerpt":"Documental Para poder considerarlo educativo según esteve, este debe cumplir los siguientes cuatro criterios: Criterio de contenido: dado que estamos hablando de un campamento religioso cuya función es adoctrinar a los niños en una creencia religiosa, no se puede considerar que lo aprendido sea moralmente irreprochable, por lo que no...","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/relaciones_tarea3",
+        "teaser": null
+      },{
+        "title": "Estilos parentales",
+        "excerpt":"¿Qué estilo te parece más adecuado? Personalmente el estilo que me parece más adecuado es el democrático, ya que a pesar de tener un alto grado de exigencia , también se tiene un alto grado de responsabilidad. Por lo que a diferencia del autoritario el hijo va a sentir que...","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/relaciones_tarea6",
+        "teaser": null
+      },{
+        "title": "Hábitos saludables (Preguntas)",
+        "excerpt":"Para trabajar el tema elegido (hábitos saludables) se le propondrán a los alumnos una serie de temas que veremos a continuación para que ellos redacten varias preguntas al respecto. Para facilitar el trabajo del alumno se usará una tabla con el principio de las preguntas posibles. Ya que la tecnología...","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/procesos1",
+        "teaser": null
+      },{
+        "title": "Hábitos saludables (Currículo)",
+        "excerpt":"La preselección de los contenidos o contraste curricular Con esta tarea vamos a explorar las posibilidades que nos ofrecen las cuestiones planteadas para trabajar determinados contenidos curriculares, por eso lo denominamos también «contraste curricular». Y hablamos de preselección porque es una primera aproximación cuya concreción definitiva dependerá principalmente de los...","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/procesos2",
         "teaser": null
       }]
